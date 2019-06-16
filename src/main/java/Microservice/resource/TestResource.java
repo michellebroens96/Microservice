a219aa96dc76029336050238ad147729d97d1374
Michelle Broens <michellebroens96@gmail.com>
2019-06-16 19:22:27 +0200

    Addedd gitignore and base REST stuff

---
+++ src/main/java/Microservice/resource/TestResource.java
@@ -0,0 +1,15 @@
+package Microservice.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/test")
+public class TestResource {
+
+    @GET
+    public Response getTest() {
+        String test = "Rest working :D";
+        return Response.ok(test).build();
+    }
+}
